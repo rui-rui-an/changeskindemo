a0e2ed8be859e3f69805d7a3ed161cf98c460336
rui-rui-an <1405613785@qq.com>
2021-11-21 17:30:13 +0800

    完成项目，提交代码

---
+++ src/views/Home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="home">
+    <div class="mybox --bgc">我盒子</div>
+    <el-select
+      @change="changeColor"
+      v-model="value"
+      placeholder="请选择背景颜色"
+    >
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      >
+      </el-option>
+    </el-select>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: '',
+      options: [{
+        value: '#0094ff',
+        label: '蓝色'
+      }, {
+        value: 'green',
+        label: '绿色'
+      }, {
+        value: 'skyblue',
+        label: '天蓝色'
+      }, {
+        value: '#006584',
+        label: '颜色4'
+      }, {
+        value: '#0082cc',
+        label: '颜色5'
+      }]
+    }
+  },
+  methods: {
+    changeColor (val) {
+      // console.log(val);
+      document.body.style.setProperty('--bgc', val);
+    },
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.home {
+  .mybox {
+    width: 50px;
+    height: 50px;
+    background-color: var(--bgc);
+  }
+}
+</style>
---
+++ src/views/Link.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="link">
+      <div class="bg_box"></div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.link{
+    .bg_box{
+        
+    }
+}
+</style>
